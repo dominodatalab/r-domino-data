46fecae39b0cd5245666be66b57bf97b8030d7d2
Dean Attali <daattali@gmail.com>
2022-12-02 14:04:55 -0500

    dont run configure script on CI

--- inst/scripts/python-setup.R
+++ inst/scripts/python-setup.R
@@ -1,5 +1,5 @@
-if (nzchar(Sys.getenv('CI'))) return()
-
-source("R/python.R")
-
-py_domino_data_install()
+if (!nzchar(Sys.getenv('CI'))) {
+  source("R/python.R")
+  
+  py_domino_data_install()
+}
